bb878aa47c48e046179e9b55626366f27da247aa
yarosla8a <yaro67819@gmail.com>
2018-04-22 15:14:54 +0300

    fixed controller

--- TestTimeTable/src/main/java/org/itstep/controller/LessonController.java
+++ TestTimeTable/src/main/java/org/itstep/controller/LessonController.java
@@ -1,57 +1,72 @@
-//package org.itstep.controller;
-//
-//import org.itstep.model.Lesson;
-//import org.itstep.service.LessonService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//@Controller
-//@RequestMapping(path="/lesson")
-//public class LessonController {
-//
-//	@Autowired
-//	 LessonService lessonService;
-//	
-//	@PostMapping( consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE}, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-//	ResponseEntity<Lesson> save(@RequestBody Lesson lesson) {
-//		if(lessonService.save(lesson) != null) {
-//			return new ResponseEntity<Lesson>(lesson, HttpStatus.OK);
-//		}
-//		return new ResponseEntity(HttpStatus.METHOD_NOT_ALLOWED);
-//	}
-//	
-//	@PutMapping( consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE} )
-//	ResponseEntity update(@RequestBody Lesson lesson) {
-//		if(lessonService.update(lesson) != null) {
-//			return new ResponseEntity(HttpStatus.OK);
-//		}
-//		return new ResponseEntity(HttpStatus.BAD_REQUEST);
-//	}
-//	
-//	@GetMapping( path = "/get-one", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE} )
-//	ResponseEntity<Lesson> getOne(@RequestParam Integer id) {
-//		Lesson lesson = lessonService.get(id);
-//		if( lesson != null) {
-//			return new ResponseEntity(lesson, HttpStatus.OK);
-//		}
-//		return new ResponseEntity(HttpStatus.BAD_REQUEST);
-//	}
-//	
-//	@GetMapping( path = "/get-all-by-time", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE} )
-//	ResponseEntity<List<Lesson>> getAll(@RequestParam Integer id) {
-//		Lesson lesson = lessonService.get(id);
-//		if( lesson != null) {
-//			return new ResponseEntity(lesson, HttpStatus.OK);
-//		}
-//		return new ResponseEntity(HttpStatus.BAD_REQUEST);
-//	}
-//}
+package org.itstep.controller;
+
+import java.util.List;
+
+import org.itstep.model.Lesson;
+import org.itstep.service.LessonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/lesson")
+public class LessonController {
+
+	@Autowired
+	LessonService lessonService;
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity<Lesson> save(@RequestBody Lesson lesson) {
+		Lesson lessonInDB = lessonService.save(lesson);
+		if (lessonInDB != null) {
+			return new ResponseEntity<Lesson>(lessonInDB, HttpStatus.OK);
+		}
+		return new ResponseEntity<Lesson>(HttpStatus.BAD_REQUEST);
+	}
+
+	@PutMapping(consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity update(@RequestBody Lesson lesson) {
+		Lesson lessonInDB = lessonService.update(lesson);
+		if (lessonInDB != null) {
+			return new ResponseEntity(lessonInDB, HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+
+	@GetMapping(path = "/get-one", consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity<Lesson> getOne(@RequestHeader Integer id) {
+		Lesson lesson = lessonService.get(id);
+		if (lesson != null) {
+			return new ResponseEntity<Lesson>(lesson, HttpStatus.OK);
+		}
+		return new ResponseEntity<Lesson>(HttpStatus.BAD_REQUEST);
+	}
+
+	@GetMapping(path = "/get-by-period", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity<List<Lesson>> findAllByPeriod(@RequestHeader Long start, @RequestHeader Long end) {
+		List<Lesson> lessons = lessonService.findAllByStartTime(start, end);
+		if (lessons != null) {
+			return new ResponseEntity<List<Lesson>>(lessons, HttpStatus.OK);
+		}
+		return new ResponseEntity<List<Lesson>>(HttpStatus.BAD_REQUEST);
+	}
+
+	@DeleteMapping(consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity delete(@RequestBody Lesson lesson) {
+
+		lessonService.delete(lesson);
+		return new ResponseEntity(HttpStatus.OK);
+
+	}
+}
--- TestTimeTable/src/main/java/org/itstep/controller/StudentController.java
+++ TestTimeTable/src/main/java/org/itstep/controller/StudentController.java
@@ -1,48 +1,73 @@
-//package org.itstep.controller;
-//
-//import org.itstep.model.Student;
-//import org.itstep.service.StudentService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//@Controller
-//@RequestMapping(path = "/student")
-//public class StudentController {
-//
-//	@Autowired
-//	StudentService studentService;
-//	
-//	@PostMapping( consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE}, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE}  )
-//	ResponseEntity<Student> save(@RequestBody Student student) {
-//		if(studentService.save(student) != null) {
-//			return new ResponseEntity<Student>(student, HttpStatus.OK);
-//		}
-//		return new ResponseEntity<Student>(HttpStatus.BAD_REQUEST);
-//	}
-//	
-//	@PutMapping( consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE} )
-//	ResponseEntity<Student> update(@RequestBody Student student) {
-//		if(studentService.update(student) != null) {
-//			return new ResponseEntity<Student>(HttpStatus.OK);
-//		}
-//		return new ResponseEntity<Student>(HttpStatus.BAD_REQUEST);
-//	}
-//	
-//	@GetMapping( path = "/get-one", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE} )
-//	ResponseEntity<Student> getOne(@RequestBody String login) {
-//		Student student = studentService.get(login);
-//		if( student != null) {
-//			return new ResponseEntity<Student>(student, HttpStatus.OK);
-//		}
-//		return new ResponseEntity<Student>(HttpStatus.BAD_REQUEST);
-//	}
-//}
+package org.itstep.controller;
+
+import java.util.List;
+
+import org.itstep.model.Student;
+import org.itstep.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/student")
+public class StudentController {
+
+	@Autowired
+	StudentService studentService;
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity<Student> save(@RequestBody Student student) {
+		Student studentInDB = studentService.save(student);
+		if (studentInDB != null) {
+			return new ResponseEntity<Student>(studentInDB, HttpStatus.OK);
+		}
+		return new ResponseEntity<Student>(HttpStatus.BAD_REQUEST);
+	}
+
+	@PutMapping(consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity update(@RequestBody Student student) {
+		Student studentInDB = studentService.update(student);
+		if (studentInDB != null) {
+			return new ResponseEntity(studentInDB, HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+
+	@GetMapping(path = "/get-one", consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity<Student> getOne(@RequestHeader String login) {
+		Student student = studentService.get(login);
+		if (student != null) {
+			return new ResponseEntity<Student>(student, HttpStatus.OK);
+		}
+		return new ResponseEntity<Student>(HttpStatus.BAD_REQUEST);
+	}
+
+	@GetMapping(path = "/get-by-group",  produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity<List<Student>> findAllByGroup(@RequestHeader String name) {
+		List<Student> students = studentService.findAllByGroup(name);
+		if (students != null) {
+			return new ResponseEntity<List<Student>>(students, HttpStatus.OK);
+		}
+		return new ResponseEntity<List<Student>>(HttpStatus.BAD_REQUEST);
+	}
+
+	@DeleteMapping(consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	ResponseEntity delete(@RequestBody Student student) {
+
+		studentService.delete(student);
+		return new ResponseEntity(HttpStatus.OK);
+
+	}
+}
